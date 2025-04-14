4e8aa5867cfe0e98d0775a04595ef9740bb12541
AliveBeing <chidvilasvarmaappani@gmail.com>
2025-04-15 00:09:55 +0530

    Completed Front-end of Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,17 +11,23 @@ import ProductCategory from './pages/ProductCategory.jsx'
 import ProductDetails from './pages/ProductDetails.jsx'
 import Cart from './pages/Cart.jsx'
 import AddAddress from './pages/AddAddress.jsx'
+import MyOrders from './pages/MyOrders.jsx'
+import SellerLogin from './components/seller/SellerLogin.jsx'
+import SellerLayout from './pages/seller/SellerLayout.jsx'
+import AddProducts from './pages/seller/AddProducts.jsx'
+import ProductList from './pages/seller/ProductList.jsx'
+import Orders from './pages/seller/Orders.jsx'
 
 
 
 function App() {
 
   const isSellerPath= useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext()
+  const {showUserLogin, isSeller} = useAppContext()
 
 
   return (
-    <div >
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
 
       {isSellerPath ? null :<Navbar/>}
       {showUserLogin ? <Login/> : null}
@@ -35,7 +41,12 @@ function App() {
           <Route path='/products/:category/:id' element={<ProductDetails/>}/>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/add-address' element={<AddAddress/>}/>
-
+          <Route path='/my-orders' element={<MyOrders/>}/>
+          <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+            <Route index element={isSeller ? <AddProducts/> : null} />
+            <Route path='product-list' element={<ProductList/>} />
+            <Route path='orders' element={<Orders/>} />
+          </Route>
         </Routes>
       </div>
       {isSellerPath ? null :<Footer/>}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,9 +32,10 @@ const Navbar = () => {
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
+            <button onClick={() => navigate('/seller')} className="border border-gray-300 px-3 py-1 rounded-full text-xs cursor-pointer opacity-80">Seller Dashboard</button>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/products'>All Products</NavLink>
-                <NavLink to='/'>Contact</NavLink>
+                {/* <NavLink to='/'>Contact</NavLink> */}
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
@@ -81,12 +82,12 @@ const Navbar = () => {
             </div>
             {/* Mobile Menu */}
             { open && (
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+            <div className={`${open ? 'flex' : 'hidden'} z-50 absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to='/' onClick={() => setOpen(false)}>Home</NavLink>
                 <NavLink to='/products' onClick={() => setOpen(false)}>All Products</NavLink>
                 <NavLink to='/' onClick={() => setOpen(false)}>Contact</NavLink>
                 {user &&
-                <NavLink to='/' onClick={() => setOpen(false)}>My Orders</NavLink>
+                <NavLink to='/my-orders' onClick={() => setOpen(false)}>My Orders</NavLink>
                 }
                 {!user ? (
                     <button 
