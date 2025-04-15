b41cdd097cd55fc5862f1acd8e7bfabc82c95c9e
AliveBeing <chidvilasvarmaappani@gmail.com>
2025-04-15 18:02:20 +0530

    Backend - connected to database completed user APIs

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import cookieParser from 'cookie-parser';
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+
+
+const app = express(); // create a new app instance
+
+const port = process.env.PORT || 3001; 
+
+
+await connectDB();
+
+
+//allow multiple origins
+const allowedOrigins=['http://localhost:5173']
+
+
+//Middleware configuration
+app.use(express.json())
+app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cookieParser())
+
+
+
+//define a simple route 
+
+app.get('/',(req,res) => {
+
+    res.send('API is working')
+})
+
+app.use('/api/user', userRouter)
+
+app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`)
+})
