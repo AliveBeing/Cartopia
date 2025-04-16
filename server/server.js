8bb964c86055e43034cd67fcfcf073fdd8333f25
AliveBeing <chidvilasvarmaappani@gmail.com>
2025-04-16 19:45:51 +0530

    Completed APIs for product completed APIs for cart

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
 
 
 const app = express(); // create a new app instance
@@ -13,7 +17,7 @@ const port = process.env.PORT || 3001;
 
 
 await connectDB();
-
+await connectCloudinary();
 
 //allow multiple origins
 const allowedOrigins=['http://localhost:5173']
@@ -35,6 +39,10 @@ app.get('/',(req,res) => {
 
 app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/address', addressRouter)
+
 
 app.listen(port, () => {
     console.log(`server running on http://localhost:${port}`)
